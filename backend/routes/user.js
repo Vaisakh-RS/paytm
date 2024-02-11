265dc07461361a6a44a9fb5ed39d3acef67b96cf
Vaisakh-RS <vaisakhrs4@gmail.com>
2024-02-11 21:38:05 +0530

    Add sigin route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -15,6 +15,7 @@ const userValidation=z.object({
 router.post("/signup",async(req,res)=>{
 
     const userDetails=req.body
+
     const result=userValidation.safeParse(userDetails)
     if(!result.success)
     {
@@ -27,17 +28,58 @@ router.post("/signup",async(req,res)=>{
             res.status(411).json({"msg":"Username already exists"})
         }
         else{
-            const newUser=await User.create(userDetails)
+            const newUser=await User.create({
+                username:userDetails.username,
+                firstName:userDetails.firstName,
+                lastName:userDetails.lastName,
+                password:userDetails.password
+
+            })
             const token=jwt.sign({
                 id:newUser._id
             },JWT_SECRET)
-            
+
             res.status(200).json({
                 message:"User created successfully",
                 token:token
             })
         }
     }
+});
+
+
+
+const signinBody=z.object({
+    username:z.string().email(),
+    password:z.string()
+});
+
+router.post("/sigin",(req,res)=>{
+    const username=req.body.username
+    const password=req.body.password
+
+    const result=signinBody.safeParse({username,password})
+    if(!result.success)
+    {
+        return res.status(411).json({message:"Invalid user input"})
+    }
+
+    const user=User.findOne({username,password})
+    if(user)
+    {   
+        const token=jwt.sign({
+            id:user._id
+        },JWT_SECRET)
+    
+        res.status(200).json({
+            token:token
+        })
+        return
+    }
+   
+
+    res.status(411).json({message:"Error signing in"})
+
 })
 
 
